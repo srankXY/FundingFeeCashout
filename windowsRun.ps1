cc21bdca75506da388de33edce53d3a5d7da3804
srankXY <srank6255@gmail.com>
2022-06-30 14:19:02 +0800

    V1.2

--- windowsRun.ps1
+++ windowsRun.ps1
@@ -5,10 +5,10 @@ function main {
     $checkDb=(ls | findstr ex.db | Measure-Object | findstr /i count |%{$_.split()[5]})
 
     if ($checkDb -ne 1){
-      FundingFeeCashout-V1.1.exe init
+      FundingFeeCashout-v1.2.exe init
     }
 
-    FundingFeeCashout-V1.1.exe
+    FundingFeeCashout-v1.2.exe
 
     Write-Host "Please press any key to exit."
     $null = [System.Console]::ReadKey()
